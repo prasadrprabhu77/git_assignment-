d108a4aa5722e0e810bd1eb81c9f82ec366e0e38
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2024-12-28 15:48:11 +0530

    Q-14 added

---
+++ Q-14.js
@@ -0,0 +1,6 @@
+let task = ["Morning Yoga","Break-fast","Attend Assignment","Live Session","Doubt Session"]
+task.shift()
+task.unshift("Morning Walk")
+task.unshift("Wash Bike")
+task.pop()
+console.log(task)
